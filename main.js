6f2d5ca827578f96408d9e91277eeca9269d899c
brunoonoo <162719226+brunoonoo@users.noreply.github.com>
2024-03-12 11:23:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://midias.correiobraziliense.com.br/_midias/jpg/2016/04/05/675x450/1_cbnfot050420160102-21057794.jpg?20220217210708?20220217210708";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2955269:1592145699/bob-esponja-1367318.jpg?f=16x9&h=574&w=1020&$p$f$h$w=a85d899"
+    
+
+}
